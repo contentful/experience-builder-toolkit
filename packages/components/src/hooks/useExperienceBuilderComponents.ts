42d6f88b35f25951922010323bab0ba09678e4b3
Ely Lucas <ely@meta-tek.net>
2023-09-19 11:11:08 -0600

    adding RichText to useExperienceBuilder

--- packages/components/src/hooks/useExperienceBuilderComponents.ts
+++ packages/components/src/hooks/useExperienceBuilderComponents.ts
@@ -1,5 +1,5 @@
 import { ComponentRegistration } from '@contentful/experience-builder';
-import { ExperienceBuilderButton } from '..';
+import { ExperienceBuilderButton, ExperienceBuilderRichText } from '..';
 import { useEffect } from 'react';
 
 export default function useExperienceBuilderComponents(
@@ -10,6 +10,10 @@ export default function useExperienceBuilderComponents(
       {
         component: ExperienceBuilderButton,
         definition: ExperienceBuilderButton.ComponentDefinition,
+      },
+      {
+        component: ExperienceBuilderRichText,
+        definition: ExperienceBuilderRichText.ComponentDefinition,
       },
     ]);
   }, []);
