562586877901d0ea3a9b157ef5fef209b48ca932
Chase Poirier <chase.poirier@contentful.com>
2023-10-26 13:14:49 -0600

    chore: add sb plugin to toolkit sb instances

--- packages/components/src/components/Heading/Heading.stories.ts
+++ packages/components/src/components/Heading/Heading.stories.ts
@@ -5,7 +5,7 @@ import { Heading, HeadingComponentDefinition } from '.';
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Example/Heading',
-  component: Heading,
+  component: ExperienceBuilderHeading,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
@@ -15,10 +15,10 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
-} satisfies Meta<typeof Heading>;
+} satisfies Meta<typeof ExperienceBuilderHeading>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof ExperienceBuilderHeading>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {
--- packages/components/src/components/Text/Text.stories.ts
+++ packages/components/src/components/Text/Text.stories.ts
@@ -5,7 +5,7 @@ import { Text, TextComponentDefinition } from '.';
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Example/Text',
-  component: Text,
+  component: ExperienceBuilderText,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
@@ -15,7 +15,7 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
-} satisfies Meta<typeof Text>;
+} satisfies Meta<typeof ExperienceBuilderText>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
