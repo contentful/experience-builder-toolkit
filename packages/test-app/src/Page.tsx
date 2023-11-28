c1a3543760c67b0ec55a3c0da284c1cda67dc263
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2023-11-28 10:39:17 -0700

    fix(test-app): restore delivery mode support [ALT-104]

--- packages/test-app/src/Page.tsx
+++ packages/test-app/src/Page.tsx
@@ -2,20 +2,40 @@ import {
   useFetchExperience,
   defineComponents,
   ExperienceRoot,
+  ExternalSDKMode,
 } from '@contentful/experience-builder';
 import { createClient } from 'contentful';
 import { useExperienceBuilderComponents } from '@contentful/experience-builder-components';
 import '@contentful/experience-builder-components/styles.css';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+
+const isPreview = window.location.search.includes('isPreview=true');
+const mode = isPreview ? 'preview' : (import.meta.env.VITE_MODE as ExternalSDKMode) || 'delivery';
+const experienceTypeId = import.meta.env.VITE_EB_TYPE_ID || 'layout';
 
 const client = createClient({
   space: import.meta.env.VITE_SPACE_ID || '',
   environment: import.meta.env.VITE_ENVIRONMENT_ID || 'master',
-  host: import.meta.env.VITE_PREVIEW_HOST || 'preview.contentful.com',
-  accessToken: import.meta.env.VITE_PREVIEW_ACCESS_TOKEN || '',
+  host: isPreview ? 'preview.contentful.com' : 'cdn.contentful.com',
+  accessToken: isPreview
+    ? import.meta.env.VITE_PREVIEW_ACCESS_TOKEN
+    : import.meta.env.VITE_ACCESS_TOKEN,
 });
 
 export default function Page() {
-  useFetchExperience({ client, mode: 'preview' });
+  const localeCode = 'en-US';
+  const { slug = '' } = useParams<{ slug: string }>();
+
+  const { experience, fetchBySlug } = useFetchExperience({ client, mode });
+
   useExperienceBuilderComponents(defineComponents);
-  return <ExperienceRoot locale="en-US" />;
+
+  useEffect(() => {
+    if (slug) {
+      fetchBySlug({ experienceTypeId, slug, localeCode });
+    }
+  }, [fetchBySlug, slug]);
+
+  return <ExperienceRoot experience={experience} locale={localeCode} />;
 }
--- packages/test-app/src/vite-env.d.ts
+++ packages/test-app/src/vite-env.d.ts
@@ -3,8 +3,9 @@
 interface ImportMetaEnv {
   readonly VITE_ENVIRONMENT_ID: string;
   readonly VITE_SPACE_ID: string;
-  readonly VITE_PREVIEW_HOST: string;
+  readonly VITE_ACCESS_TOKEN: string;
   readonly VITE_PREVIEW_ACCESS_TOKEN: string;
+  readonly VITE_EB_TYPE_ID: string;
 }
 
 interface ImportMeta {
