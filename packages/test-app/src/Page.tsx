ffad3a61fe20d846d132c427034261dcca28ad2a
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2023-11-28 14:26:53 -0700

    fix(test-app): switch IIFE to asyncFetch() and include hasFetched logic to prevent redundant fetch

--- packages/test-app/src/Page.tsx
+++ packages/test-app/src/Page.tsx
@@ -8,7 +8,7 @@ import { createClient } from 'contentful';
 import { useExperienceBuilderComponents } from '@contentful/experience-builder-components';
 import '@contentful/experience-builder-components/styles.css';
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 const isPreview = window.location.search.includes('isPreview=true');
 const mode = isPreview ? 'preview' : (import.meta.env.VITE_MODE as ExternalSDKMode) || 'delivery';
@@ -26,17 +26,23 @@ const client = createClient({
 export default function Page() {
   const localeCode = 'en-US';
   const { slug } = useParams<{ slug: string }>();
+  const hasFetched = useRef(false);
 
   const { experience, fetchBySlug } = useFetchExperience({ client, mode });
 
   useExperienceBuilderComponents(defineComponents);
 
   useEffect(() => {
-    (async () => {
+    const asyncFetch = async () => {
       if (slug) {
         await fetchBySlug({ experienceTypeId, slug, localeCode });
       }
-    })();
+    };
+
+    if (!hasFetched.current) {
+      asyncFetch();
+      hasFetched.current = true;
+    }
   }, [fetchBySlug, slug]);
 
   return <ExperienceRoot experience={experience} locale={localeCode} />;
