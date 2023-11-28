aa61fc9f135fc86865f68b3c18510080e222ec31
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2023-11-28 10:03:56 -0700

    fix(test-app): load experience automagically

--- packages/test-app/src/Page.tsx
+++ packages/test-app/src/Page.tsx
@@ -2,50 +2,20 @@ import {
   useFetchExperience,
   defineComponents,
   ExperienceRoot,
-  ExternalSDKMode,
 } from '@contentful/experience-builder';
-import React, { useEffect } from 'react';
 import { createClient } from 'contentful';
-import { useParams } from 'react-router-dom';
 import { useExperienceBuilderComponents } from '@contentful/experience-builder-components';
 import '@contentful/experience-builder-components/styles.css';
-
-const experienceTypeId = import.meta.env.VITE_EB_TYPE_ID || 'layout';
-
-const isPreview = window.location.search.includes('isPreview=true');
-
-const mode = isPreview ? 'preview' : ((import.meta.env.VITE_MODE || 'delivery') as ExternalSDKMode);
 
 const client = createClient({
   space: import.meta.env.VITE_SPACE_ID || '',
   environment: import.meta.env.VITE_ENVIRONMENT_ID || 'master',
-  host: isPreview ? 'preview.contentful.com' : 'cdn.contentful.com',
-  accessToken: isPreview
-    ? import.meta.env.VITE_PREVIEW_ACCESS_TOKEN
-    : import.meta.env.VITE_ACCESS_TOKEN,
+  host: import.meta.env.VITE_PREVIEW_HOST || 'preview.contentful.com',
+  accessToken: import.meta.env.VITE_PREVIEW_ACCESS_TOKEN || '',
 });
 
-const Page: React.FC = () => {
-  // Configure the Content Preview URL to include "/{entry.fields.slug}?isPreview=true"
-  const { slug = '/' } = useParams<{ slug: string }>();
-  const localeCode = 'en-US';
-
-  const { experience, fetchBySlug } = useFetchExperience({
-    client,
-    mode,
-  });
-
-  useEffect(() => {
-    fetchBySlug({ experienceTypeId, slug, localeCode });
-  }, [fetchBySlug, localeCode, slug]);
-
+export default function Page() {
+  useFetchExperience({ client, mode: 'preview' });
   useExperienceBuilderComponents(defineComponents);
-
-  if (!experience) {
-    return null;
-  }
-
-  return <ExperienceRoot experience={experience} locale={localeCode} />;
-};
-
-export default Page;
+  return <ExperienceRoot locale="en-US" />;
+}
--- packages/test-app/src/vite-env.d.ts
+++ packages/test-app/src/vite-env.d.ts
@@ -3,10 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_ENVIRONMENT_ID: string;
   readonly VITE_SPACE_ID: string;
-  readonly VITE_ACCESS_TOKEN: string;
+  readonly VITE_PREVIEW_HOST: string;
   readonly VITE_PREVIEW_ACCESS_TOKEN: string;
-  readonly VITE_EB_TYPE_ID: string;
-  readonly VITE_MODE: string;
 }
 
 interface ImportMeta {
