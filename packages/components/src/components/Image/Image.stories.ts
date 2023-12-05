9d8ce6e290a5a3dfee4940de7d5c90ff1320b829
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2023-12-05 12:33:31 -0700

    fix: url -> src in Image.stories

--- packages/components/src/components/Image/Image.stories.ts
+++ packages/components/src/components/Image/Image.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-// import constants from '@/utils/constants';
+import constants from '@/utils/constants';
 import { Image, ImageComponentDefinition } from '.';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -23,7 +23,7 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {
   args: {
-    // url: constants.placeholderImage,
+    src: constants.placeholderImage,
     width: 300,
   },
 };
