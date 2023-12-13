44c2e25921162815e3519e37228bcd9aef0579e6
Adrian Meyer <8539634+primeinteger@users.noreply.github.com>
2023-12-13 10:32:19 -0700

    fix: add new constant VISUAL_EDITOR_CONTAINER_ID

--- packages/visual-editor/src/renderApp.tsx
+++ packages/visual-editor/src/renderApp.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { VisualEditorRoot } from './components/VisualEditorRoot';
 import './global.css';
+import { VISUAL_EDITOR_CONTAINER_ID } from '@contentful/experience-builder-core';
 
-ReactDOM.createRoot(document.getElementById('cf-visual-editor')!).render(
+ReactDOM.createRoot(document.getElementById(VISUAL_EDITOR_CONTAINER_ID)!).render(
   <React.StrictMode>
     <VisualEditorRoot />
   </React.StrictMode>
